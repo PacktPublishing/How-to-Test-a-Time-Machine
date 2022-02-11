edb33e9e007e0adcac3ebfd4b78e5c38e60271f9
Noemi Elisa <{ID}+{username}@users.noreply.github.com>
2022-02-11 18:47:46 +0100

    wrong file corrected

--- Chapter4/changeURL.js
+++ Chapter4/changeURL.js
@@ -1,15 +1,11 @@
-describe('changeResponseTest', function(){ 
-
-    it('changesResponseFromNumbersAPI', function() { 
-    
-     const staticResponse = { 
-      statusCode: 200, 
-      body: { "text": "42 modified response", "number": 42, "found": true, "type": "trivia"}, 
-      headers :{'Access-Control-Allow-Origin': '*'} 
-     } 
-    
-    cy.intercept('http://numbersapi.com/42?json', staticResponse) 
-    cy.visit('http://numbersapi.com/42?json') 
-    
+describe('changeURL', function(){ 
+    it('changesCodeForImage', function() { 
+    cy.intercept('https://github.com/', (req) => {
+         req.reply((res) => {
+             res.body = '<img src="https://random-d.uk/api/261.jpg" alt="TestingTimeMachines">'
+         })
+     }).as('newURL')
+    cy.visit('https://github.com/')
+    cy.wait('@newURL').its('response.body').should('include', 'TestingTimeMachines')
     })
 })
