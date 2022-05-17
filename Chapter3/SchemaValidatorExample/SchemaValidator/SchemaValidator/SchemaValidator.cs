74c5338de28a4731e01ca51a7dc9e0f56c065cf0
Noemi Elisa <{ID}+{username}@users.noreply.github.com>
2022-05-17 08:14:11 +0200

    Chapter 3 v1

---
+++ Chapter3/SchemaValidatorExample/SchemaValidator/SchemaValidator/SchemaValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using System.Collections.Generic;
+
+namespace SchemaValidatorSrc
+{
+	public class SchemaValidator
+	{
+		// Retrieving schema from before and object from the response...
+		public bool validate(JObject toValidate, JSchema schema)
+		{
+			IList<string> messages;
+			bool valid = toValidate.IsValid(schema, out messages);
+			foreach (string message in messages)
+			{
+				Console.WriteLine(message);
+			}
+			/* Instead of writing in console we may want to raise an exception here with the message list*/
+			return valid;
+		}
+
+		static public void Main(String[] args)
+		{
+			Console.WriteLine("Schema Validator, use test");
+		}
+	}
+}
