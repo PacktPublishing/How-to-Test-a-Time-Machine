488284011ebe6471da85a021aa19c0dc319466df
Noemi Elisa <{ID}+{username}@users.noreply.github.com>
2022-09-27 16:05:40 +0200

    Adding poco

---
+++ Chapter10/DoorAuto.py
@@ -0,0 +1,27 @@
+# -*- encoding=utf8 -*-
+__author__ = "Noemi"
+from airtest.core.api import * 
+
+auto_setup(__file__) 
+
+import time
+from poco.drivers.unity3d import UnityPoco 
+from poco.drivers.unity3d.device import UnityEditorWindow 
+
+dev=UnityEditorWindow()  
+addr = ('', 5001) 
+poco = UnityPoco(addr, device=dev) 
+#note: use device=UnityEditorWindow() to test in unity  
+# dev = connect_device('Android:///<serialno>') to test in Android 
+
+vr = poco.vr 
+poco = poco() 
+position = poco('Door').attr('position') 
+vr.rotateObject(-10, 0, 0, 'XRig', 'Camera Offset', 0.5) 
+vr.objectLookAt('Door', 'XRig', 'Camera Offset', 5) 
+count = 0 
+while(!vr.checkIfUnityFinished() && count < 10): 
+    time.sleep(2) 
+    count = count + 1 
+poco.click() 
+assert poco('Door').attr('position')!= position 
