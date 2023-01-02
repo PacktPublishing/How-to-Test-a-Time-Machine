8ff5dc671caa6e898adf3184af9f29b6ca6af85a
Noemi Elisa <{ID}+{username}@users.noreply.github.com>
2023-01-03 00:02:31 +0100

    Fixing iterator name

--- Chapter08/datacurator.py
+++ Chapter08/datacurator.py
@@ -10,34 +10,34 @@ result = {}
 # Data optimization -> get the maximum number of steps and of differences
 oldest = 0
 maxSteps = 0
-for i in dataDictionary:
-    createdDate = datetime.datetime.fromisoformat(dataDictionary[i]['CreatedDate'])
+for data in dataDictionary:
+    createdDate = datetime.datetime.fromisoformat(dataDictionary[data]['CreatedDate'])
     currentDate = datetime.datetime.now()
     difference = currentDate - createdDate
     daysOfDifference = difference.days
-    dataDictionary[i]['daysDiff'] = daysOfDifference
+    dataDictionary[data]['daysDiff'] = daysOfDifference
     if daysOfDifference > oldest:
         oldest = daysOfDifference
-    if dataDictionary[i]['Steps'] > maxSteps:
-        maxSteps = dataDictionary[i]['Steps']
+    if dataDictionary[data]['Steps'] > maxSteps:
+        maxSteps = dataDictionary[data]['Steps']
 # Save the data in the new dictionary
-for i in dataDictionary:
-        result[i] = {}
-        passes = dataDictionary[i]['passes']
-        fails = dataDictionary[i]['fails']
+for data in dataDictionary:
+        result[data] = {}
+        passes = dataDictionary[data]['passes']
+        fails = dataDictionary[data]['fails']
         # Data optimization - get the percentage of passes
         passingTest = (passes / (fails + passes)) * 100
         # Data optimization - get the percentage of test age
-        testAge = (dataDictionary[i]['daysDiff'] / oldest) * 100
-        priorityTest = dataDictionary[i]['Priority']
+        testAge = (dataDictionary[data]['daysDiff'] / oldest) * 100
+        priorityTest = dataDictionary[data]['Priority']
         # Data optimization - get the percentage of number steps
-        numSteps = (dataDictionary[i]['Steps'] / maxSteps) * 100
+        numSteps = (dataDictionary[data]['Steps'] / maxSteps) * 100
         # Save the new values
-        result[i]['Priority'] = float(priorityTest)
-        result[i]['Age'] = float(testAge)
-        result[i]['Steps'] = float(numSteps)
-        result[i]['Passing'] = float(passingTest)
-        result[i]['Expected'] = float(dataDictionary[i]['LastResult'])
+        result[data]['Priority'] = float(priorityTest)
+        result[data]['Age'] = float(testAge)
+        result[data]['Steps'] = float(numSteps)
+        result[data]['Passing'] = float(passingTest)
+        result[data]['Expected'] = float(dataDictionary[data]['LastResult'])
 # Save to the new Data File
 dataFrame = pd.DataFrame.from_dict(result, orient='index')
 dataFile = open('newData.csv', 'w')
