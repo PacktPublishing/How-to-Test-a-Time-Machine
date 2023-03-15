3fe9f357da5c0527f602240f3cf864ce44b29fca
TheTestLynx <16527196+TheTestLynx@users.noreply.github.com>
2023-03-15 09:21:14 +0100

    Chapter06 missing files

---
+++ Chapter06/UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		[Priority(2)]
+		public void TestMethod1()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod]
+		[Priority(1)]
+		public void TestMethod2()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod]
+		[Priority(3)]
+		public void TestMethod3()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod]
+		[Priority(1)]
+		public void TestMethod4()
+		{
+			Assert.Fail();
+		}
+	}
+}
---
+++ Chapter06/test_execution.py
@@ -0,0 +1,9 @@
+import os
+from yaml import load, safe_load, Loader
+from git import Repo
+with open('instructions.yml') as stream:
+    data = safe_load(stream)
+    for step in data['Steps']:
+        print(step['name'])
+        print(step['log'])
+        os.system(step['instruction'])
